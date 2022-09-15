fea649c4029d651cb47709111c5bde4bce76ab8c
Eric <eocebreiro@gmail.com>
2022-09-16 02:42:19 +0900

    fixed bugs

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -4,27 +4,18 @@ import { Link } from "react-router-dom";
 export const PokemonCard = ({ name, url, index, image }) => {
   return (
     <div className="col-md-3 col-sm-6 mb-5">
-      <Link to={`pokemon/${index}`}>
+      <Link className="card-link-custom" to={`pokemon/${index}`}>
         <div className="card card-custom">
           <h5 className="card-header">#{index}</h5>
           <div>{}</div>
           <img
             style={{ width: "6rem", height: "6rem" }}
-            className="card-img-top rounded mx-auto mt-2"
+            className="card-img-top rounded mx-auto mt-2 pokemon-card-img"
             src={image}
             alt={"Pokemon #" + index + " Image"}
           />
           <div className="card-body mx-auto">
-            <h6 className="card-title">
-              {name
-                .toLowerCase()
-                .split(" ")
-                .map(
-                  (letter) =>
-                    letter.charAt(0).toUpperCase() + letter.substring(1)
-                )
-                .join(" ")}
-            </h6>
+            <h6 className="card-title text-capitalize">{name}</h6>
           </div>
         </div>
       </Link>
