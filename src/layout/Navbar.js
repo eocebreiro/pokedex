647448fedd5f4d79a468ddab37ce9f2198d8362d
Eric <eocebreiro@gmail.com>
2022-09-16 02:40:09 +0900

    fixed logo link

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a className="navbar-brand col-sm-3 col-md-2 mr-0 text-center">Pokedex</a>
+    <nav className="ps-2 pe-2 navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+      <Link to="/" className="navbar-brand col-sm-3 col-md-2 mr-0 text-center">
+        Pokedex
+      </Link>
     </nav>
   );
 };
