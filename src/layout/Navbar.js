58c5e4400f71c30c835c253372d07efcbd7037b1
Eric <eocebreiro@gmail.com>
2022-09-16 23:18:58 +0900

    added a logo image

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import pokeballImg from "../img/pokeball.png";
 
 export const Navbar = () => {
   return (
     <nav className="ps-2 pe-2 navbar navbar-expand-md navbar-dark bg-dark fixed-top">
       <Link to="/" className="navbar-brand col-sm-3 col-md-2 mr-0 text-center">
-        Pokedex
+        P
+        <img
+          className="pokeball-logo-icon"
+          src={pokeballImg}
+          alt="pokeball logo icon"
+        />
+        kedex
       </Link>
     </nav>
   );
