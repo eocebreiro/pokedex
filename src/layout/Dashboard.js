80c43b3fb5651621705ce1516bd0ffa76abae647
Eric C <eocebreiro@gmail.com>
2022-09-16 14:37:08 +0900

    added a search option for pokemon number

--- src/layout/Dashboard.js
+++ src/layout/Dashboard.js
@@ -1,16 +1,68 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import { PokemonList } from "../components/PokemonList";
 import { SearchBar } from "../components/SearchBar";
 
 export const Dashboard = ({ pokemonList }) => {
+  const [formData, setFormData] = useState({
+    search: "",
+  });
+  const { search } = formData;
+
+  const [list, setList] = useState(pokemonList);
+
+  const onChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const getList = async () => {
+    let searchList = pokemonList
+      .filter((pokemon) => {
+        if (
+          pokemon.name.toLowerCase().includes(search.toLowerCase()) ||
+          pokemon.index === search
+        ) {
+          return true;
+        } else {
+          return false;
+        }
+      })
+      .map((pokemon) => {
+        return pokemon;
+      });
+
+    setList(searchList);
+  };
+
+  useEffect(() => {
+    getList();
+  }, [search]);
   return (
     <Fragment>
       <div className="container ">
-        <SearchBar />
+        <div className="pt-4 pb-4">
+          <form>
+            <input
+              placeholder="Pokemon Name or Number"
+              name="search"
+              value={search}
+              onChange={(e) => onChange(e)}
+              autoComplete="off"
+              className="form-control mx-auto search-custom"
+              style={{
+                backgroundColor: "white transparent",
+                height: "1.75em",
+                width: "95%",
+                borderRadius: "10px",
+                opacity: "0.8",
+                fontSize: "1.75em",
+              }}
+            />
+          </form>
+        </div>
         <div className="row">
           <div className="col">
-            <PokemonList pokemon={pokemonList} />
+            <PokemonList pokemon={list} />
           </div>
         </div>
       </div>
