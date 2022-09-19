36d0103419cbbf18f37ec424a998fb31331d9f85
Eric <eocebreiro@gmail.com>
2022-09-20 00:13:14 +0900

    fixed the display for smaller phones

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -5,7 +5,7 @@ import noimage from "../img/noimage.png";
 
 export const PokemonCard = ({ name, url, index, image }) => {
   return (
-    <div className="col-md-3 col-sm-6 mb-5">
+    <div className="col-6 col-md-3 col-sm-6 mb-5">
       <Link className="card-link-custom" to={`pokemon/${index}`}>
         <div className="card card-custom">
           <div className="card-header">
@@ -21,7 +21,9 @@ export const PokemonCard = ({ name, url, index, image }) => {
             />
           </div>
           <div className="card-body mx-auto">
-            <h5 className="card-title text-capitalize mb-0 ">{name}</h5>
+            <h5 className="card-title card-title-custom text-capitalize mb-0 ">
+              {name}
+            </h5>
             <span className="d-flex justify-content-center">{"#" + index}</span>
           </div>
         </div>
